aac6cd0b1883c2cff5d7a80f7ac1309750ec06f6
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-05 23:07:29 -0300

    Solução final 1 a 10.

--- functions.js
+++ functions.js
@@ -1094,28 +1094,27 @@ var listaProdutos = [
 ]
 
 // 1. Quantidade total de itens em estoque (somatória das quantidades de todos os produtos)
-const quantidadeEstoque = ((lista) => {
+const ex1 = ((lista) => {
     let total = 0
     lista.map(produto => {
         total += produto.qtdEstoque
-    })
+    })  
     console.log(`1. Quantidade total de itens em estoque: ${total}`)
-    return total;
 })
-quantidadeEstoque(listaProdutos)
+ex1(listaProdutos)
 
 // 2. Quantidade total de itens em destaque (somatória das quantidades dos itens marcados como "emDestaque : sim")
-const quantidadeDestaque = ((lista) => {
-    let total = 0
-    lista.map(produto => {
-        if (produto.emDestaque === "sim"){
-            total += produto.qtdEstoque
+const qtdEmDestaque = (produtos) => {
+    let soma = 0
+    for (var i = 0; i < produtos.length; i++){
+        if (produtos[i].emDestaque === "sim") {
+            soma = soma + produtos[i].qtdEstoque
         }
-    })
-    console.log(`2. Quantidade total de itens em destaque: ${total}`)
-    return total;
-})
-quantidadeDestaque(listaProdutos)
+    }
+    console.log(`2. Quantidade total de itens em destaque ${soma}`)
+    return soma
+}
+qtdEmDestaque(listaProdutos)
 
 // 3. Quantidade total de itens disponíveis (similar ao anterior)
 const quantidadeDisponível = ((lista) => {
@@ -1155,64 +1154,64 @@ const valorInventario = ((lista) => {
 let totalValorInventario = valorInventario(listaProdutos)
 
 // 6. Produto mais caro da loja (bem como seu departamento - considere apenas o preço dele)
-const produtoMaisCaro = ((lista) => {
-    let maxPreco = Number.NEGATIVE_INFINITY
-    let prod = null
-    lista.map(produto => {
-        if (produto.preco > maxPreco){
-            maxPreco = produto.preco
-            prod = produto
+const produtoMaiscaro = (produtos) => {
+    let maiorPreco = produtos[0].preco
+    for (var i = 1; i < produtos.length; i++){
+        if (produtos[i].preco > maiorPreco) {
+            maiorPreco = produtos[i].preco
         }
-    })
-    console.log(`6. Produto mais caro da loja: "${prod.descricao}" do Departamento ${prod.departamento.nomeDepto}`)
-    return prod;
-})
-produtoMaisCaro(listaProdutos)
+    }
+    let prodMaiorPreco
+    let prodMaiorPrecoDepto
+    for (var i = 1; i < produtos.length; i++){
+        if (produtos[i].preco === maiorPreco){
+            prodMaiorPreco = produtos[i].descricao
+            prodMaiorPrecoDepto = produtos[i].departamento.nomeDepto
+        }
+    }
+    console.log(`6. Produto mais caro da loja é ${prodMaiorPreco} do departamento ${prodMaiorPrecoDepto} que tem o preço ${maiorPreco}`)
+    return maiorPreco
+}
+produtoMaiscaro(listaProdutos)
 
 // 7. Produto mais barato da loja (bem como seu departamento - considere apenas o preço dele)
-const produtoMaisBarato = ((lista) => {
-    let minPreco = Number.POSITIVE_INFINITY
-    let prod = null
-    lista.map(produto => {
-        if (produto.preco < minPreco){
-            minPreco = produto.preco
-            prod = produto
+function ex7(maisBarato) {
+    maisBarato = listaProdutos.map(produto => produto.preco).reduce((a, b) => Math.min(a, b))
+    nomeMaisBarato = listaProdutos.map(produto => {
+        if (maisBarato == produto.preco) {
+            nomeProdutoMaisBarato = produto.descricao
+            nomeDepartamento = produto.departamento.nomeDepto
         }
     })
-    console.log(`7. Produto mais barato da loja: "${prod.descricao}" do Departamento ${prod.departamento.nomeDepto}`)
-    return prod;
-})
-produtoMaisBarato(listaProdutos)
+    console.log(`7. O produto ${nomeProdutoMaisBarato} é o item mais barato do estoque. Ele custa R$ ${maisBarato} e pertence ao departamento de ${nomeDepartamento}`)
+}
+ex7()
 
 // 8. Produto de estoque mais valioso (considere o preço multiplicado pela quantidade e também apenas EM ESTOQUE)
-const produtoMaisValioso = ((lista) => {
-    let maxValor = Number.NEGATIVE_INFINITY
-    let prod = null
-    lista.map(produto => {
-        if (produto.qtdEstoque > 0 && (produto.preco * produto.qtdEstoque) > maxValor){
-            maxValor = (produto.preco * produto.qtdEstoque)
-            prod = produto
+function ex8(maisValioso) {
+    maisValioso = listaProdutos.map(produto => produto.preco * produto.qtdEstoque).reduce((a, b) => Math.max(a, b))
+    nomeMaisValioso = listaProdutos.map(produto => {
+        if (maisValioso == produto.preco * produto.qtdEstoque) {
+            nomeProdutoMaisValioso = produto.descricao
+            nomeDepartamento = produto.departamento.nomeDepto
         }
-    })
-    console.log(`8. Produto de estoque mais valioso: "${prod.descricao}" do Departamento ${prod.departamento.nomeDepto}`)
-    return prod;
-})
-produtoMaisValioso(listaProdutos)
+    }) 
+    console.log(`8. O produto ${nomeProdutoMaisValioso} é o item mais valioso do estoque. Todo o estoque custa R$ ${maisValioso} e pertence ao departamento de ${nomeDepartamento}`)
+}
+ex8()
 
 // 9. Produto em estoque menos valioso (considere o preço multiplicado pela quantidade e também apenas EM ESTOQUE)
-const produtoMenosValioso = ((lista) => {
-    let minValor = Number.POSITIVE_INFINITY
-    let prod = null
-    lista.map(produto => {
-        if (produto.qtdEstoque > 0 && (produto.preco * produto.qtdEstoque) < minValor){
-            minValor = (produto.preco * produto.qtdEstoque)
-            prod = produto
+function ex9(menosValioso) {
+    menosValioso = listaProdutos.map(produto => produto.preco * produto.qtdEstoque).reduce((a,b) => a*b ? Math.min(a,b) : (a ? a : b))
+    nomeMenosValioso = listaProdutos.map(produto => {
+        if (menosValioso == produto.preco * produto.qtdEstoque ) {
+            nomeProdutoMenosValioso = produto.descricao
+            nomeDepartamento = produto.departamento.nomeDepto
         }
-    })
-    console.log(`9. Produto em estoque menos valioso: "${prod.descricao}" do Departamento ${prod.departamento.nomeDepto}`)
-    return prod;
-})
-produtoMenosValioso(listaProdutos)
+    }) 
+    console.log(`9. O produto ${nomeProdutoMenosValioso} é o item menos valioso do estoque. Todo o estoque custa R$ ${menosValioso} e pertence ao departamento de ${nomeDepartamento}`)
+}
+ex9()
 
 // 10. Valor do ticket médio dos produtos da empresa (basicamente o valor total do inventário dividido pelo número de itens - considere TODOS os produtos, porém considere apenas 1 exemplar por produto)
 const ticketMedio = ((lista) => {
@@ -1222,84 +1221,3 @@ const ticketMedio = ((lista) => {
     return ticket;
 })
 ticketMedio(listaProdutos)
-
-// 11. Somatória de itens por departamento (você deverá retornar um objeto contendo o nome do departamento e o total de itens nele - Novamente considere os produtos “EM ESTOQUE” - e é apenas a somatória da quantidade de itens)
-// DÚVIDA: De que forma deve ser o output? Uma lista de objetos como [{nomeDpto: Somátoria Itens}, {nomeDpto: Somátoria Itens}, ...}] ? Ou com o 'departamento' no input e a saída apenas com 1 objeto com a somatória de itens daquele departamento?
-const itensPorDepartamento = ((lista) => {
-    let departamentos = {}
-    lista.map(produto => {
-        let dpto = produto.departamento.nomeDepto
-        if (!Object.keys(departamentos).includes(dpto)){
-            departamentos = {... departamentos, [dpto]:produto.qtdEstoque}
-        } else {
-            departamentos[dpto] += produto.qtdEstoque
-        }
-    })
-    console.log("11. Somatória de itens por departamento: " + JSON.stringify(departamentos))
-    return departamentos;
-})
-itensPorDepartamento(listaProdutos)
-
-// 12. Valor total do inventário por departamento (similar ao item anterior - considere TODOS os produtos)
-// DÚVIDA: Mesma dúvida do itens 11
-const inventarioPorDepartamento = ((lista) => {
-    let departamentos = {}
-    lista.map(produto => {
-        let dpto = produto.departamento.nomeDepto
-        if (!Object.keys(departamentos).includes(dpto)){
-            departamentos = {... departamentos, [dpto]:(produto.preco * produto.qtdEstoque)}
-        } else {
-            departamentos[dpto] += (produto.preco * produto.qtdEstoque)
-        }
-    })
-    console.log("12. Valor total do inventário por departamento: " + JSON.stringify(departamentos))
-    return departamentos;
-})
-let invPorDepartamento = inventarioPorDepartamento(listaProdutos)
-
-// 13. Ticket médio por departamento (similar ao item anterior, porém retornando uma lista de objetos que contenha o nome do departamento e o seu ticket médio)
-const ticketMedioPorDepartamento = ((lista) => {
-    let departamentos = []
-    Object.keys(invPorDepartamento).map((dpto) => {
-        let quant = 0
-        lista.map(produto => {
-            if (produto.departamento.nomeDepto === dpto){
-                quant ++
-            }
-        })
-        departamentos = [... departamentos, {[dpto]: (invPorDepartamento[dpto]/quant)}]
-    })
-    console.log("13. Ticket médio por departamento: " + JSON.stringify(departamentos))
-    return departamentos
-})
-ticketMedioPorDepartamento(listaProdutos)
-
-// 14. Departamento mais valioso (qual o departamento que tem a maior somatória dos valores dos itens - Considere todos os departamentos)
-const departamentoMaisValioso = ((lista) => {
-    let departamento = null
-    let maxValor = Number.NEGATIVE_INFINITY
-    Object.keys(invPorDepartamento).map((dpto) => {
-        if (invPorDepartamento[dpto] > maxValor){
-            maxValor = invPorDepartamento[dpto]
-            departamento = dpto
-        }
-    })
-    console.log(`14. Departamento mais valioso: ${departamento}`)
-    return departamento
-})
-departamentoMaisValioso(listaProdutos)
-
-// 15. Departamento menos valioso (similar ao anterior)
-const departamentoMenosValioso = ((lista) => {
-    let departamento = null
-    let minValor = Number.POSITIVE_INFINITY
-    Object.keys(invPorDepartamento).map((dpto) => {
-        if (invPorDepartamento[dpto] < minValor){
-            minValor = invPorDepartamento[dpto]
-            departamento = dpto
-        }
-    })
-    console.log(`15. Departamento menos valioso: ${departamento}`)
-    return departamento
-})
-departamentoMenosValioso(listaProdutos)
